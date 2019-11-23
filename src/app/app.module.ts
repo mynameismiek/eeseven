e1bdddc30333023474a915aa2603ef4fb055e6a3
mynameismiek <mynameismiek@gmail.com>
2019-11-23 14:03:17 -0500

    removing comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -73,10 +73,3 @@ import { UserService } from './user.service';
   providers: [EpicSevenDbService, UserService]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
